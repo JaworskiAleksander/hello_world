3f23a7f93812d36f3851874afdd7b93d722efd03
Aleksander Jaworski <aleck156@gmail.com>
2020-05-14 10:46:00 +0200

    created IndexView class, set up template_name to index.html template

--- hello_world_CBV/basic_app/views.py
+++ hello_world_CBV/basic_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 
 # Create your views here.
-
+class IndexView(TemplateView):
+    # template_name is a class-based attribute inherited from Template_View
+    # it tells which template should be rendered when calling out this class as view // .as_view()
+    template_name = 'index.html'
 
