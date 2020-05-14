1e58799557a91956bf2ebb1bdad30926a2f86246
Aleksander Jaworski <aleck156@gmail.com>
2020-05-14 10:58:14 +0200

    created get_context_data method in IndexView class to handle context dictionary

--- hello_world_CBV/basic_app/views.py
+++ hello_world_CBV/basic_app/views.py
@@ -6,4 +6,9 @@ class IndexView(TemplateView):
     # template_name is a class-based attribute inherited from Template_View
     # it tells which template should be rendered when calling out this class as view // .as_view()
     template_name = 'index.html'
-
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Message of the day'
+        return context
+    
