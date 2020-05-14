b51328f0922914eeb96970d1318c6c4929e38cb1
Aleksander Jaworski <aleck156@gmail.com>
2020-05-14 15:03:09 +0200

    added comment on how related_name works when querying over ForeignKey in template tags at school_details.html

--- hello_world_CBV/basic_app/models.py
+++ hello_world_CBV/basic_app/models.py
@@ -14,6 +14,9 @@ class Student(models.Model):
     name = models.CharField(max_length=256)
     age = models.PositiveIntegerField()
     school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
+    # related_name - foreign key is how a student points at which school they're related to
+    # related_name is how an instance of School object can query over students that have pointers
+    # directing at that instance of School class
 
     def __str__(self):
         return self.name
