6010df1f5121231bfd64d8d785b7bf0416cf4af1
Aleksander Jaworski <aleck156@gmail.com>
2020-05-14 10:42:56 +0200

    removed CBView class and HttpResponse import

--- hello_world_CBV/basic_app/views.py
+++ hello_world_CBV/basic_app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
 
 # Create your views here.
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class-Based View")
 
 
