96efa559dde8ec4b34672c0d8334be5aaf707ae1
Aleksander Jaworski <aleck156@gmail.com>
2020-05-15 10:18:39 +0200

    code cleaning - removed empty space

--- hello_world_CBV/basic_app/urls.py
+++ hello_world_CBV/basic_app/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('create/', views.SchoolCreateView.as_view(), name='create'),
     path('update/<int:pk>', views.SchoolUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', views.SchoolDeleteView.as_view(), name='delete'),
-
 ]
