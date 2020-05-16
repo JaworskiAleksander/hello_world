c944869838685370a91fc27aefa7c558abcccb1b
Aleksander Jaworski <aleck156@gmail.com>
2020-05-16 09:34:34 +0200

    registered StudentListView nder student_list name in urlpatterns

--- hello_world_CBV/basic_app/urls.py
+++ hello_world_CBV/basic_app/urls.py
@@ -7,8 +7,14 @@ app_name = 'basic_app'
 
 urlpatterns = [
     path('', views.SchoolListView.as_view(), name='list'),
+
+    # School links
     path('<int:pk>', views.SchoolDetailView.as_view(), name='detail'),
     path('create/', views.SchoolCreateView.as_view(), name='create'),
     path('update/<int:pk>', views.SchoolUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', views.SchoolDeleteView.as_view(), name='delete'),
+
+    # Students ... should't this be split into two, separate applications?!
+    path('students/', views.StudentListView.as_view(),'student_list'),
+    
 ]
