b653e76ac30415e43d862a7e64e2440a1973620f
Aleksander Jaworski <aleck156@gmail.com>
2020-05-16 10:52:54 +0200

    registered StudentDeleteView under student_delete name in urlpatterns

--- hello_world_CBV/basic_app/urls.py
+++ hello_world_CBV/basic_app/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('students/<int:pk>', views.StudentDetailView.as_view(), name='student_detail'),
     path('students/create/', views.StudentCreateView.as_view(), name='student_create'),
     path('students/update/<int:pk>', views.StudentUpdateView.as_view(), name='student_update'),
-    
+    path('students/delete/<int:pk>', views.StudentDeleteView.as_view(), name='student_delete'),    
 ]
